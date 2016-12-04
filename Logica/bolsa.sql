dbdc8e61d5156615e78029ee69c5cc40c5898ac7
garyDav <gary.2810.dav@gmail.com>
2016-12-04 01:25:14 -0400

    añadi imagenes

--- Logica/bolsa.sql
+++ Logica/bolsa.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 01-12-2016 a las 22:32:33
--- Versión del servidor: 10.1.9-MariaDB
--- Versión de PHP: 7.0.1
+-- Tiempo de generación: 04-12-2016 a las 06:21:46
+-- Versión del servidor: 10.1.13-MariaDB
+-- Versión de PHP: 7.0.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -100,6 +100,23 @@ CREATE TABLE `categoria` (
   `logo` varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `name`, `logo`) VALUES
+(8, 'Medicos', '1480629450.png'),
+(9, 'Albañiles', '1480629847.jpeg'),
+(10, 'Arquitectos', '1480630141.jpeg'),
+(11, 'Pintores', '1480630343.jpeg'),
+(12, 'Abogados', '1480630500.png'),
+(13, 'Diseñador', '1480630695.jpeg'),
+(14, 'Docentes', '1480630831.jpeg'),
+(15, 'Choferes', '1480631145.jpeg'),
+(16, 'Lavanderas', '1480631243.jpeg'),
+(17, 'Tecnicos', '1480631377.jpeg'),
+(18, 'Veterinarios', '1480645964.jpeg');
+
 -- --------------------------------------------------------
 
 --
@@ -128,6 +145,20 @@ CREATE TABLE `empresa` (
   `photo` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
   `detail` text COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `empresa`
+--
+
+INSERT INTO `empresa` (`id`, `id_categoria`, `name`, `email`, `address`, `phone`, `cell`, `photo`, `detail`) VALUES
+(10, 8, 'San Carlos', 'medicos@gmail.com', 'Av. Olimpo', '64-62318', 73400123, '1480631688.png', 'La clinica San Carlos se dedica a la al diagnostico de los pacientes'),
+(11, 10, 's&n', 'arquitectos@gmail.com', 'Antofagasta # 85', '64-58974', 68945782, '1480633228.jpeg', 'Se dedica al diseño de su vivienda, y entre otros.'),
+(12, 11, 'Pintura Bonita', 'pintores@gmail.com', 'Antofagasta # 23', '64-58958', 68945745, '1480633405.jpg', 'Realizan todo tipo de pintura tradicional, simantica.'),
+(13, 12, 'Bufeg de Abogados', 'abogados@gmail.com', 'Manuel Molina #12', '64-58364', 68945285, '1480634112.jpg', 'Se dedica a casos de penalistas y otros.'),
+(14, 14, 'ITSEC', 'docentes@gmail.com', 'Manuel Molina #89', '64-52568', 68946985, '1480634455.jpeg', 'La institucion se dedica a contratar docentes de exelencia en sus diferentes carreras.'),
+(15, 15, 'Dorado', 'dorado@gmail.com', 'Ostria Gutierrez', '64-65894', 75698246, '1480634892.jpg', 'Se necesita un chofer categria c  buen sueldo'),
+(16, 9, 'Constructora', 'constructora@gmail.com', 'Destacamento # 25', '64-25686', 73542135, '1480636678.jpeg', 'Busca maestro albañil y ayudantes'),
+(17, 18, 'Procam', 'veterinarios@gmail.com', 'Paraguay', '64-67659', 73465321, '1480646137.jpeg', 'La empresa procam se dedica a la atencion de mascotas.');
 
 -- --------------------------------------------------------
 
@@ -162,6 +193,19 @@ CREATE TABLE `trabajo` (
   `fec_lim` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `trabajo`
+--
+
+INSERT INTO `trabajo` (`id`, `id_empresa`, `name`, `detail`, `salary`, `fec`, `fec_in`, `fec_lim`) VALUES
+(9, 10, 'nicolas', 'Busco Traumatologo para la clinica San Carlos', 8000, '2016-12-01 22:48:09', '2016-12-02', '2016-12-10'),
+(10, 10, 'Medico General', 'See necesita un Medico General eficiente', 7500, '2016-12-01 22:55:06', '2016-12-04', '2016-12-09'),
+(11, 15, 'Adrian Castellanos', 'Necesita chofer categoria c para viajes interdepartamentales', 8000, '2016-12-01 23:32:06', '2016-12-01', '2016-12-08'),
+(12, 12, 'Pintor', 'Se busca pintor profecional y un ayudante', 2000, '2016-12-01 23:36:50', '2016-12-01', '2016-12-12'),
+(13, 13, 'Penalista', 'Se busca abagado penalista buen sueldo', 10000, '2016-12-01 23:41:26', '2016-12-01', '2016-12-15'),
+(14, 11, 'Arquitectos', 'Se necesita un arquitecto con experiencia o sin experiencia', 5000, '2016-12-01 23:50:14', '2016-12-01', '2016-12-16'),
+(15, 16, 'Maestro albañil', 'busca maestro albañil', 3500, '2016-12-02 00:03:43', '2016-12-01', '2016-12-16');
+
 -- --------------------------------------------------------
 
 --
@@ -183,7 +227,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `last_name`, `email`, `password`, `cell`, `fec`) VALUES
-(3, 'Nicolas', 'Quispe', 'nick_8391@hotmail.com', 'b686682c584d3bb40e819d7eb67212b9e44ad99b', 68638233, '2016-11-24 00:36:32');
+(3, 'Nicolas', 'Quispe', 'nick_8391@hotmail.com', 'b686682c584d3bb40e819d7eb67212b9e44ad99b', 68638233, '2016-11-24 00:36:32'),
+(5, 'Juan', 'Perez', 'juan@gmail.com', 'b686682c584d3bb40e819d7eb67212b9e44ad99b', 75792276, '2016-12-04 00:26:05');
 
 --
 -- Índices para tablas volcadas
@@ -237,7 +282,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT de la tabla `departamento`
 --
@@ -247,7 +292,7 @@ ALTER TABLE `departamento`
 -- AUTO_INCREMENT de la tabla `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT de la tabla `sucursal`
 --
@@ -257,12 +302,12 @@ ALTER TABLE `sucursal`
 -- AUTO_INCREMENT de la tabla `trabajo`
 --
 ALTER TABLE `trabajo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Restricciones para tablas volcadas
 --
